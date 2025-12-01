8e782fbbd933453e689521a0e715b18581c02adb
Ibrahim <159002102+SKTWIR@users.noreply.github.com>
2025-12-01 15:36:59 +0100

    ajout d'un docker file pour permettre de lancer le taff

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import dataRoutes from "./routes/data.js";
 import adminRoutes from "./routes/admin.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express();
 app.use(express.json());
@@ -19,6 +22,16 @@ app.use(
 app.use("/auth", authRoutes);
 app.use("/data", dataRoutes);
 app.use("/admin", adminRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir le dossier frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
 app.listen(3000, () => {
     console.log("🔥 Server running on port 3000");
